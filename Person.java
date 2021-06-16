96080a69cbafe16f8c0260d421e90badb06aec28
Divyansh123a <43573674+Divyansh123a@users.noreply.github.com>
2021-06-16 20:28:39 +0530

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,31 @@
+public class Person {
+    
+    	// Fill the code here
+    	private String name;
+    	private char gender;
+    	private int age;
+    	public void setName(String name)
+    	{
+    	    this.name = name;
+    	}
+    	public void setGender(char gender)
+    	{
+    	    this.gender = gender;
+    	}
+    	public void setAge(int age)
+    	{
+    	    this.age = age;
+    	}
+    	public String getName()
+    	{
+    	    return this.name;
+    	}
+    	public char getGender()
+    	{
+    	    return this.gender;
+    	}
+    	public int getAge()
+    	{
+    	    return this.age;
+    	}
+	}
